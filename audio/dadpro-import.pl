87344534bec7888c96cb836cb71eaa066b5677c2
Fred Gleason <fredg@paraelsystems.com>
2018-08-02 12:17:25 -0400

    2018-08-01 Fred Gleason <fredg@paravelsystems.com> 	* Modified as-played reports to work correctly with UTF-8 strings.

--- audio/dadpro-import.pl
+++ audio/dadpro-import.pl
@@ -7,6 +7,10 @@
 #
 # (C) Copyright 2015 Fred Gleason <fredg@paravelsystems.com>
 #
+#  This script requires the Perl 'Xbase' module, available at:
+#
+#    http://www.dcs.ed.ac.uk/home/perl5/Xbase.html
+#
 #   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License version 2 as
 #   published by the Free Software Foundation.
